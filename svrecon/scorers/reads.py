b329086188f649706c5a073628216e9e344932b1
Tony Cui <tacui@mit.edu>
2026-08-20 07:59:46 -0400

    patched read scoring bug where passing segment validations were overwritten

--- svrecon/scorers/reads.py
+++ svrecon/scorers/reads.py
@@ -91,7 +91,6 @@ class ReadScorer(Scorer):
     def __init__(self, config, chroms):
         super().__init__(config, chroms)
         self.read_error_threshold = config.read_error_threshold
-        self.min_read_support = config.min_read_support
         self.max_reads_per_site = config.max_reads_per_site
         self.bam_reader = BamReader(config.bam)
 
@@ -111,7 +110,7 @@ class ReadScorer(Scorer):
         lowest_pass_error = 1.0
         lowest_error = 1.0
         validating_seq = None
-        segment_validation_results = []
+        best_validation_results = []  # the adopted read's segments on a pass, else the last read's
 
         read_results = run_read_edlib(query.sequence, reads, self.read_error_threshold)
         for read_res in read_results:
@@ -123,6 +122,9 @@ class ReadScorer(Scorer):
                 passed = True
                 lowest_pass_error = read_res.error
                 validating_seq = read_res.matched_target_sequence
+                best_validation_results = segment_validation_results
+            elif not passed:
+                best_validation_results = segment_validation_results
 
         if passed:
             status, reason = SubseqStatus.PASS, SubseqReason.PASS
@@ -134,4 +136,4 @@ class ReadScorer(Scorer):
         return QueryValidationInput(source=ValidationSource.READS, passed=passed,
                                     status=status, reason=reason,
                                     lowest_pass_error=lowest_pass_error, lowest_error=lowest_error,
-                                    validating_seq=validating_seq, segment_results=segment_validation_results)
+                                    validating_seq=validating_seq, segment_results=best_validation_results)
